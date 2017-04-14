fb584efee7f7f95502e3a7e324c9ce2bfcecc07f
chris <ceaves110@gmail.com>
2017-04-14 14:12:05 -0600

    chris changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,6 +41,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->FFT1->addItem("16384", QVariant(16384));
     ui->FFT1->addItem("32768", QVariant(32768));
     ui->FFT1->addItem("65536", QVariant(65536));
+
     ui->WSize->addItem("Rectangular");
     ui->WSize->addItem("Blackman");
     ui->WSize->addItem("Flat Top");
@@ -48,7 +49,19 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->WSize->addItem("Hamming");
     ui->Theme1->addItem("Dark");
     ui->Theme1->addItem("White");
-    
+
+    ui->AVG1->addItem("1",QVariant(1));
+    ui->AVG1->addItem("2",QVariant(2));
+    ui->AVG1->addItem("3",QVariant(3));
+    ui->AVG1->addItem("4",QVariant(4));
+    ui->AVG1->addItem("5",QVariant(5));
+    ui->AVG1->addItem("6",QVariant(6));
+    ui->AVG1->addItem("7",QVariant(7));
+    ui->AVG1->addItem("8",QVariant(8));
+    ui->AVG1->addItem("9",QVariant(9));
+    ui->AVG1->addItem("10",QVariant(10));
+
+
     inSetup = false;
 
     ui->CF2->addItem("GHz");
@@ -98,7 +111,6 @@ void MainWindow::setGUIValues()
     ui->CF1->setText(QString::number(CF));
     ui->AB1->setText(QString::number(AB));
     ui->Span1->setText(QString::number(S));
-    ui->AVG1->setText(QString::number(numberOfAverages));
 }
 
 void MainWindow::setXAxis()
@@ -490,20 +502,7 @@ void MainWindow::on_AB1_editingFinished()
     }
 }
 
-void MainWindow::on_AVG1_editingFinished()
-{
-    int tAvg = ui->AVG1->text().toInt();
-    if(tAvg > 0 && tAvg < 10)
-    {
-        endRunningThread();
-        numberOfAverages = tAvg;
-        refreshPlotting();
-    }
-    else
-    {
-        QMessageBox::about(this, "Incorrect Value", "Enter a number 0 and 10");
-    }
-}
+
 
 void MainWindow::on_CF2_currentTextChanged(const QString &arg1)
 {
@@ -552,4 +551,26 @@ void MainWindow::on_Theme1_currentIndexChanged(const QString &arg1)
         ui->customPlot1->graph(0)->setPen(QPen(QColor(30, 119, 227)));
         ui->customPlot1->graph(0)->setLineStyle((QCPGraph::LineStyle)2);
     }
+}
+void MainWindow::on_Export_clicked()
+{
+  QDateTime date(QDateTime::currentDateTime());
+  QString dateString = date.toString();
+  QString s = dateString.replace(QRegExp(" "), "_");
+
+   ui->widget->show();
+    ui->centralWidget->grab().save("Spectrum"+ s + ".png");
+}
+
+void MainWindow::on_AVG1_currentTextChanged(const QString &arg1)
+{
+    int tAvg = ui->AVG1->currentText().toInt();
+
+    if(tAvg > 0 && tAvg <= 10)
+    {
+        endRunningThread();
+        numberOfAverages = tAvg;
+        refreshPlotting();
+    }
+
 }
