87b8aa2b512935342154ad9e7611a243524c7df0
Brandon <dannyseltenreich@ymail.com>
2017-04-26 23:46:13 -0500

    UI changes and fixed CF drop down to auto-update the plot. Also added those naughty global variables...sorry Dan

--- mainwindow.h
+++ mainwindow.h
@@ -46,6 +46,7 @@ private Q_SLOTS:
     void on_CF1_editingFinished();
     void on_AB1_editingFinished();
     void on_Span1_editingFinished();
+    void on_CF2_unitchange(void);
     void on_CF2_currentTextChanged(const QString &arg1);
     void on_Span2_currentTextChanged(const QString &arg1);
     void on_WSize_currentIndexChanged(int index);
@@ -71,10 +72,11 @@ private:
     int numPoints;
     int tempNumPoints;
     int numberOfAverages;
+    double maxFrequency;
     double maxFrequency1;
     double maxFrequency2;
     double maxPoint;
-    double cfMhz;
+    //double cfMhz;
     double spanMhz;
     bool firstRun;
     bool isLinear;
